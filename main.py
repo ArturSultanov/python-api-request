d5004782d4d7beb3414096be68902d000adfee2e
Artur Sultanov <sultanov.personal@gmail.com>
2023-06-27 21:21:31 +0200

    update

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import requests
+
+response = requests.get("https://api.github.com/users/ArturSultanov/repos")
+my_repos = response.json()
+# print(my_repos)
+
+for repo in my_repos:
+    print(f"Repository Name: {repo['name']}\nRepository URL: {repo['html_url']}\n")
